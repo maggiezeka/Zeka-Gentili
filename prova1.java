ff14c0314614a9711574a00059ca1247f41cc68d
Maggie Zeka <Maggie Zeka@LAPTOP-0L69JUOF>
2020-05-08 18:00:02 +0200

    second commit

--- prova1.java
+++ prova1.java
@@ -1,6 +1,6 @@
  import java.util.*;
 public class prova1 {
 	public static void main(String[] args) {
-		System.out.println("Ciao Mondo sono qui");
+		System.out.println("Ciao Mondo sono quiiiiiii");
 	}
 }
